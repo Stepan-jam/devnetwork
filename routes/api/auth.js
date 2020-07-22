bb71c41927ffe3349714f836039cae93e5024084
Stepan <stepan.jamalyan@gmail.com>
2020-07-22 11:43:30 +0600

    feat: add user authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,96 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const auth = require("../../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../../models/User");
 
 // @route  GET api/auth
 // @desc   Test route
 // @access Public (we don't need token)
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-module.exports = router
+// @route  POST api/auth
+// @desc   Authenticate user & get token
+// @access Public (we don't need token)
+router.post(
+  "/",
+  [
+    // 1
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req); // 2
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email }); // 3
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Generate jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
+
+// 1
+/* check([field, message])
+field (optional): a string or an array of strings of field names to validate against.
+message (optional): an error message to use when failed validators don't specify a message. */
+
+// 2
+/* validationResult(req) Express-validator method. Extracts the validation errors 
+from a request and makes them available in a Result object. */
+
+// 3
+/* To select data from a collection in MongoDB, we can use the findOne() method.
+The findOne() method returns the first occurrence in the selection. */
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,100 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
 
-// @route  GET api/users
-// @desc   Test route
+const User = require("../../models/User");
+
+// @route  POST api/users
+// @desc   Register user
 // @access Public (we don't need token)
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    // 1
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req); // 2
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports = router
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email }); // 3
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Generate jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
+
+// 1
+/* check([field, message])
+field (optional): a string or an array of strings of field names to validate against.
+message (optional): an error message to use when failed validators don't specify a message. */
+
+// 2
+/* validationResult(req) Express-validator method. Extracts the validation errors 
+from a request and makes them available in a Result object. */
+
+// 3
+/* To select data from a collection in MongoDB, we can use the findOne() method.
+The findOne() method returns the first occurrence in the selection. */
